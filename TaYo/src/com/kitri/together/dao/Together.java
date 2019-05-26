6c18388ec1f8abf55c2242bb4e7322edabdbcb54
sohyoun <mongswell69@naver.com>
2019-05-26 22:25:17 +0900

    05/26

--- TaYo/src/com/kitri/together/dao/Together.java
+++ TaYo/src/com/kitri/together/dao/Together.java
@@ -1,5 +1,6 @@
 package com.kitri.together.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.kitri.dto.TTLeaderDTO;
@@ -7,7 +8,10 @@ import com.kitri.dto.TTLeaderDTO;
 public class Together {
 	//함께타요 일정 리스트 불러오기
 	public List<TTLeaderDTO> selectPlan() {
-		return null;
+		
+		List<TTLeaderDTO> list = new ArrayList<>();
+		
+		return list;
 		
 	}
 }
