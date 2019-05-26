7ac892cb260ebed3dce98fc38edb86685cf9267e
sohyoun <mongswell69@naver.com>
2019-05-27 02:33:19 +0900

    함께타요 exerd/dto 수정

--- TaYo/src/com/kitri/dto/TTLeaderDTO.java
+++ TaYo/src/com/kitri/dto/TTLeaderDTO.java
@@ -2,9 +2,7 @@ package com.kitri.dto;
 
 public class TTLeaderDTO {
 	private int tripSeq;		//함께타요id
-	private String partyEmail;	//참여자이메일
 	private int tripNum;		//여행정원
-	private int partyOK;		//가입수락여부
 	public TripBasicDTO tripBasicDTO = new TripBasicDTO();
 	
 	public int getTripSeq() {
@@ -13,24 +11,14 @@ public class TTLeaderDTO {
 	public void setTripSeq(int tripSeq) {
 		this.tripSeq = tripSeq;
 	}
-	public String getPartyEmail() {
-		return partyEmail;
-	}
-	public void setPartyEmail(String partyEmail) {
-		this.partyEmail = partyEmail;
-	}
+
 	public int getTripNum() {
 		return tripNum;
 	}
 	public void setTripNum(int tripNum) {
 		this.tripNum = tripNum;
 	}
-	public int getPartyOK() {
-		return partyOK;
-	}
-	public void setPartyOK(int partyOK) {
-		this.partyOK = partyOK;
-	}
+
 	
 	
 }
