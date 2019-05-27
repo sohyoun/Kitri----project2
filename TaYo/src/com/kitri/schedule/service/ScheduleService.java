5962a2a547e02fcd264102c7da0e90a626e8db3d
jwleejwlee <ppzzqqmm@naver.com>
2019-05-27 16:46:22 +0900

    19.05.27	Fixed code (areacode -> areaCode)

--- TaYo/src/com/kitri/schedule/service/ScheduleService.java
+++ TaYo/src/com/kitri/schedule/service/ScheduleService.java
@@ -25,7 +25,7 @@ public class ScheduleService {
 		urlString.append("listYN="+"Y"+"&");										// 목록 구분 = (Y=목록, N=개수)
 		urlString.append("arrange="+"A"+"&");										// 정렬 = (A=제목순, B=조회순, C=수정일순, D=생성일순)
 		urlString.append("contentTypeId="+"12"+"&");								// 관광타입(관광지, 숙박 등) ID = 12 (관광지)
-		urlString.append("areacode="+Integer.parseInt(location)+"&");				// 지역코드
+		urlString.append("areaCode="+Integer.parseInt(location)+"&");				// 지역코드
 		urlString.append("keyword="+URLEncoder.encode((place).trim(), "UTF-8"));	// 검색할 키워드 (한글인 경우 UTF-8 인코딩 필수)
 		
 		URL url = new URL(urlString.toString());
