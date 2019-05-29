b9c0c8b2cc8f420c17b0946c0eee29e21ca07bb0
yeeooni <12.12kimiyeon@gmail.com>
2019-05-29 17:24:06 +0900

    2019. 05. 29
    select all db query

---
+++ TaYo/src/com/kitri/admin/model/dao/AdminDAO.java
@@ -0,0 +1,13 @@
+package com.kitri.admin.model.dao;
+import java.util.List;
+
+import com.kitri.dto.AdminDTO;
+import com.kitri.dto.MemberDetailDTO;
+
+public interface AdminDAO {
+
+	AdminDTO selectByEmail(String email);
+	
+	List<MemberDetailDTO> selectAll();
+
+}
--- TaYo/src/com/kitri/admin/model/service/AdminService.java
+++ TaYo/src/com/kitri/admin/model/service/AdminService.java
@@ -1,6 +1,9 @@
 package com.kitri.admin.model.service;
+import java.util.List;
+
 import com.kitri.admin.model.dao.AdminDAOImpl;
 import com.kitri.dto.AdminDTO;
+import com.kitri.dto.MemberDetailDTO;
 
 public class AdminService {
 
@@ -12,4 +15,10 @@ public class AdminService {
 	
 		return AdminDAOImpl.getAdminDAO().selectByEmail(email);
 	}
+	
+	public List<MemberDetailDTO> memeberAll(String email, String address, String addressDetail, String gender, String grade){
+		return AdminDAOImpl.getAdminDAO().selectAll();
+	}
+	
+	
 }
