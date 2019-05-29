b4d43dc66a681e6453e2625b0ee9bd17b61e775c
hth0893 <hth0893@naver.com>
2019-05-29 22:16:17 +0900

    trip DB select commit

--- TaYo/src/com/kitri/tripbasic/dao/TripBasicDao.java
+++ TaYo/src/com/kitri/tripbasic/dao/TripBasicDao.java
@@ -5,75 +5,83 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.kitri.db.impl.BasicDao;
 import com.kitri.dto.*;
+import com.kitri.tripdetail.dao.TripDetailDao;
 import com.kitri.util.DBClose;
 import com.kitri.util.DBConnection;
 
 
-public class TripBasicDao implements BasicDao<TripBasicDTO>{
+public class TripBasicDao {
 	static TripBasicDao tripBasicDao;
 	static{
 		tripBasicDao = new TripBasicDao();
 	}	
 
-	public TripBasicDao getInstance() {
+	private TripBasicDao() {}
+	public static TripBasicDao getInstance() {
 		return tripBasicDao;
 	}
 	
 
-	@Override
+
 	public List<TripBasicDTO> selectAll() {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		
-		List<TripBasicDTO> list = new ArrayList<TripBasicDTO>();
+		List<TripBasicDTO> basiclist = new ArrayList<TripBasicDTO>();
 		try {
 			conn = DBConnection.makeConnection();
-			String sql = " select trip_seq, email, trip_title, trip_theme, trip_season, start_date, end_date, viewcount, likecount \n" + 
-					" from trip_Basic;";
+			String sql = "select trip_seq, email, trip_title, trip_theme, trip_season, start_date, end_date, viewcount, likeCount ,lastupdate\n" + 
+					"from trip_basic";
 			pstmt = conn.prepareStatement(sql);
 			rs = pstmt.executeQuery();
+			
 			while (rs.next()) {
-				int tripSeq = rs.getInt("trip_seq");
+				int trip_seq = rs.getInt("trip_seq");
 				String email = rs.getString("email");
 				String tripTitle = rs.getString("trip_title");
 				String tripTheme = rs.getString("trip_theme");
 				String tripSeason = rs.getString("trip_season");
-				
 				Date startDate = rs.getDate("start_date");
 				Date endDate = rs.getDate("end_date");
 				int viewCount= rs.getInt("viewcount");
 				int likeCount = rs.getInt("likecount");
 				Date lastUpDate =rs.getDate("lastUpDate");
-				List<TripDetailDTO> detailList= new ArrayList<TripDetailDTO>();
-				
-				TripBasicDTO dto = new TripBasicDTO(tripSeq, email, tripTitle, tripTheme, tripSeason, startDate, endDate, viewCount, likeCount, lastUpDate, detailList);
-				list.add(dto);
+				TripBasicDTO dto = new TripBasicDTO(trip_seq, email, tripTitle, tripTheme, tripSeason, startDate, endDate, viewCount, likeCount, lastUpDate, null);
+				basiclist.add(dto);
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
 			DBClose.close(conn, pstmt, rs);
 		}
-		return list;
+		return basiclist;
 	}
 
-	@Override
 	public int insert(TripBasicDTO dto) {
 		return -1;
 	}
 
-	@Override
-	public String select(TripBasicDTO id) {
+	
+	public String select(int id) {
 		return null;
 	}
 
 
 
 	public static void main(String[] args) {
-		TripBasicDao dao = new TripBasicDao();
-		dao.selectAll();
-	}
+		
+		List<TripBasicDTO> basicList = TripBasicDao.getInstance().selectAll();
+		for(TripBasicDTO basicDto : basicList) {
+			System.out.println("====================");
+			System.out.println(basicDto.toString());
+			
+			List<TripDetailDTO> detailList = TripDetailDao.getInstance().select(basicDto.getTripSeq());
+			for(TripDetailDTO detailDto : detailList) {	
+			System.out.println(detailDto.toString());
+			}
+			System.out.println("====================");
+		}
+	}//end main
 }
--- TaYo/src/com/kitri/tripbasic/service/TripBasicService.java
+++
@@ -1,5 +0,0 @@
-package com.kitri.tripbasic.service;
-
-public class TripBasicService {
-	
-}
