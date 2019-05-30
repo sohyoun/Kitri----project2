a91069ab2330d9a4a83bb93cbc2a87f57a52588f
hth0893 <hth0893@naver.com>
2019-05-30 17:03:52 +0900

    json commit

--- TaYo/src/com/kitri/api/tour/service/TourResionCodeService.java
+++ TaYo/src/com/kitri/api/tour/service/TourResionCodeService.java
@@ -56,7 +56,7 @@ public class TourResionCodeService {
 	        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 	        conn.setRequestMethod("GET");
 	        conn.setRequestProperty("Content-type", "application/json");
-	        System.out.println("Response code: " + conn.getResponseCode());
+//	        System.out.println("TourResionCodeService Response code: " + conn.getResponseCode());
 	        BufferedReader rd;
 	        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
 	            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
