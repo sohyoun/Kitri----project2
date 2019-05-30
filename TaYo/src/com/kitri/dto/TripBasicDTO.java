268a86c59a4e2518a391daf60a564d76ca7e2cf9
sohyoun <mongswell69@naver.com>
2019-05-30 17:50:58 +0900

    tripbasicDTO에서 tripDetailDTO new로 연결

--- TaYo/src/com/kitri/dto/TripBasicDTO.java
+++ TaYo/src/com/kitri/dto/TripBasicDTO.java
@@ -19,15 +19,17 @@ public class TripBasicDTO {
 	private int likeCount;		//추천수
 	private Date lastUpDate;		//최종수정일
 	private String isComplete;		//완료여부
-	List<TripDetailDTO> detailList;	//상세일정리스트
+	TripDetailDTO tripDetailDTO = new TripDetailDTO(); //상세일정리스트
 
 	
 	public TripBasicDTO() {
 		super();
 	}
 
+
 	public TripBasicDTO(int tripSeq, String email, String tripTitle, String tripTheme, String tripSeason, int tripNum,
-			Date startDate, Date endDate, int viewCount, int likeCount, Date lastUpDate, String isComplete, List<TripDetailDTO> detailList) {
+			Date startDate, Date endDate, int viewCount, int likeCount, Date lastUpDate, String isComplete,
+			TripDetailDTO tripDetailDTO) {
 		super();
 		this.tripSeq = tripSeq;
 		this.email = email;
@@ -41,100 +43,147 @@ public class TripBasicDTO {
 		this.likeCount = likeCount;
 		this.lastUpDate = lastUpDate;
 		this.isComplete = isComplete;
-		this.detailList = detailList; 
+		this.tripDetailDTO = tripDetailDTO;
 	}
 
-	
+
 	@Override
 	public String toString() {
 		return "TripBasicDTO [tripSeq=" + tripSeq + ", email=" + email + ", tripTitle=" + tripTitle + ", tripTheme="
-				+ tripTheme + ", tripSeason=" + tripSeason + ", startDate=" + startDate + ", endDate=" + endDate
-				+ ", viewCount=" + viewCount + ", likeCount=" + likeCount + ", lastUpDate=" + lastUpDate + ", isComplete=" + isComplete
-				+ ", detailList=" + detailList + "]";
+				+ tripTheme + ", tripSeason=" + tripSeason + ", tripNum=" + tripNum + ", startDate=" + startDate
+				+ ", endDate=" + endDate + ", viewCount=" + viewCount + ", likeCount=" + likeCount + ", lastUpDate="
+				+ lastUpDate + ", isComplete=" + isComplete + ", tripDetailDTO=" + tripDetailDTO + "]";
 	}
-	
-	public List<TripDetailDTO> getDetailList() {
-		return detailList;
-	}
-	public void setDetailList(List<TripDetailDTO> detailList) {
-		this.detailList = detailList;
-	}
-	public Date getLastUpDate() {
-		return lastUpDate;
-	}
-	public void setLastUpDate(Date lastUpDate) {
-		this.lastUpDate = lastUpDate;
-	}
+
+
 	public int getTripSeq() {
 		return tripSeq;
 	}
+
+
 	public void setTripSeq(int tripSeq) {
 		this.tripSeq = tripSeq;
 	}
+
+
 	public String getEmail() {
 		return email;
 	}
+
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
+
 	public String getTripTitle() {
 		return tripTitle;
 	}
+
+
 	public void setTripTitle(String tripTitle) {
 		this.tripTitle = tripTitle;
 	}
+
+
 	public String getTripTheme() {
 		return tripTheme;
 	}
+
+
 	public void setTripTheme(String tripTheme) {
 		this.tripTheme = tripTheme;
 	}
+
+
 	public String getTripSeason() {
 		return tripSeason;
 	}
+
+
 	public void setTripSeason(String tripSeason) {
 		this.tripSeason = tripSeason;
 	}
+
+
 	public int getTripNum() {
 		return tripNum;
 	}
+
+
 	public void setTripNum(int tripNum) {
 		this.tripNum = tripNum;
 	}
+
+
 	public Date getStartDate() {
 		return startDate;
 	}
+
+
 	public void setStartDate(Date startDate) {
 		this.startDate = startDate;
 	}
+
+
 	public Date getEndDate() {
 		return endDate;
 	}
+
+
 	public void setEndDate(Date endDate) {
 		this.endDate = endDate;
 	}
+
+
 	public int getViewCount() {
 		return viewCount;
 	}
+
+
 	public void setViewCount(int viewCount) {
 		this.viewCount = viewCount;
 	}
+
+
 	public int getLikeCount() {
 		return likeCount;
 	}
+
+
 	public void setLikeCount(int likeCount) {
 		this.likeCount = likeCount;
 	}
+
+
+	public Date getLastUpDate() {
+		return lastUpDate;
+	}
+
+
+	public void setLastUpDate(Date lastUpDate) {
+		this.lastUpDate = lastUpDate;
+	}
+
+
 	public String getIsComplete() {
 		return isComplete;
 	}
+
+
 	public void setIsComplete(String isComplete) {
 		this.isComplete = isComplete;
 	}
-	public List<TripDetailDTO> getTripDetailDTO() {
-		return detailList;
+
+
+	public TripDetailDTO getTripDetailDTO() {
+		return tripDetailDTO;
 	}
-	public void setTripDetailDTO(List<TripDetailDTO> tripDetailDTO) {
-		this.detailList = tripDetailDTO;
+
+
+	public void setTripDetailDTO(TripDetailDTO tripDetailDTO) {
+		this.tripDetailDTO = tripDetailDTO;
 	}
+
+	
 }
