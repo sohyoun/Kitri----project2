cdd40123ecbc0fc87ca07078a6cf90abb70ee23e
sohyoun <mongswell69@naver.com>
2019-06-14 14:36:09 +0900

    끝

--- TaYo/src/com/kitri/schedule/controller/ScheduleController.java
+++ TaYo/src/com/kitri/schedule/controller/ScheduleController.java
@@ -122,9 +122,9 @@ public class ScheduleController extends HttpServlet {
 			request.setAttribute("notice", notice);
 			MoveURL.forward(request, response, "/tayoschedule/savePlanResult.jsp");
 		} else if ("joinTT".equals(act)) {
-//			backendController.joinTT(request, response);
+			backendController.joinTT(request, response);
 		} else if ("outTT".equals(act)) {
-//			backendController.outTT(request, response);
+			backendController.outTT(request, response);
 		}
 	}
 	
